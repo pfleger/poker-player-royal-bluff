fac2d0812673d5ff049303dd1531237e981a3c35
Piotr Fleger <piotr.fleger@capgemini.com>
2018-10-04 10:07:07 +0200

    dummy bet request

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 1;
+        return 10;
     }
 
     public static void showdown(JsonElement game) {
