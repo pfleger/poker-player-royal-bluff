d4b4639edb5780b1723178ffc0df570b2707a798
Piotr Fleger <piotr.fleger@capgemini.com>
2018-10-04 10:34:19 +0200

    dummy

---
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -0,0 +1,15 @@
+package org.leanpoker.player;
+
+public class GameState {
+    String tournament_id;
+     /*     "game_id":"550da1cb2d909006e90004b1",
+          "round":0,
+          "bet_index":0,
+          "small_blind":10,
+          "orbits":0",
+          "in_action":0,
+          "dealer":0,
+          "community_cards":[],
+          "current_buy_in":0,
+          "pot":0*/
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,27 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+
+        JsonArray players = request.getAsJsonObject().getAsJsonArray("players");
+        int current_buy_in = getAsInt(request, "current_buy_in");
+        int pot = getAsInt(request, "pot");
+
         return 1000;
+    }
+
+    private static int getAsInt(JsonElement request, String current_buy_in1) {
+        return getJsonElement(request, current_buy_in1).getAsInt();
+    }
+
+    private static JsonElement getJsonElement(JsonElement request, String current_buy_in1) {
+        return request.getAsJsonObject().get(current_buy_in1);
     }
 
     public static void showdown(JsonElement game) {
